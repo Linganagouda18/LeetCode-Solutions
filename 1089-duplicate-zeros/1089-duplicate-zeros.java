52e768ff08f5910e43fe808e14634c77c84bf86c
Linganagouda18 <linganagoudapatil18@gmail.com>
2025-07-30 01:24:41 +0530

    Time: 1 ms (98.91%), Space: 44.7 MB (78.82%) - LeetHub

---
+++ 1089-duplicate-zeros/1089-duplicate-zeros.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void duplicateZeros(int[] arr) {
+        int[] res = new int[arr.length];
+        int index = 0 ;
+        for(int i = 0 ; i < arr.length && index < arr.length ;i++){
+            if(arr[i] == 0){
+                res[index++] = 0;
+               if(index < arr.length){
+                res[index++] = 0;
+               }
+            }else{
+                  res[index++] = arr[i];
+            }
+    }
+        for(int i = 0 ; i < arr.length;i++){
+                arr[i] = res[i];
+            }
+}
+}
